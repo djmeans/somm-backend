e687d5fb63ffbb23ff2309e958ec0e96c2d8ab32
Dillon Means <dillon.j.means@gmail.com>
2018-07-12 12:45:03 -0600

    fixed delete

--- routes/regions.js
+++ routes/regions.js
@@ -6,4 +6,14 @@ router.get(('/'), (req, res) => {
     queries.getAllRegions().then(regions => res.json({ regions }))
 })
 
+router.get(('/:id'),(req, res)=>{
+    queries.getRegionById(req.params.id).then(region => res.json({ region }))
+})
+
+router.delete(('/:id'),(req, res) =>{
+    queries.delete(req.params.id).then(() => {
+        response.status(204).json({deleted: true});
+    }).catch(next);
+})
+
 module.exports = router;
